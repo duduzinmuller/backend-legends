e3b21402149b9d165e173d9d6972b759dc7b2a02
Eduardo <eduardomuller989@gmail.com>
2025-03-03 13:08:01 -0400

    feat: add CustomerService

---
+++ src/services/customerService.ts
@@ -0,0 +1,40 @@
+import { Order } from "@prisma/client";
+import { prisma } from "../utils/prisma";
+
+interface CustomerServiceProps {
+    id: string;
+    email: string;
+    name: string;
+    phone?: string;
+    orders: Order[];
+}
+
+export const CustomerService = async ({
+    id,
+    name,
+    email,
+    phone,
+    orders,
+}: CustomerServiceProps) => {
+    try {
+        const customer = await prisma.customer.create({
+            data: {
+                id,
+                name,
+                email,
+                phone,
+                orders: {
+                    create: orders.map((order) => ({
+                        id: order.id,
+                        status: order.status,
+                        totalAmount: order.totalAmount,
+                        createdAt: order.updatedAt,
+                    })),
+                },
+            },
+        });
+        return customer;
+    } catch (error) {
+        console.error(error);
+    }
+};
